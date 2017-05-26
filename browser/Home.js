dc07a29b8401d0bba8b91e5b127111a93e215577
Gladys Um <gladysum@gmail.com>
2017-05-26 10:48:45 -0400

    basic homepage

--- browser/Home.js
+++ browser/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
 
 const Home = () => (
   <div className="home">
     <div className="home-col-left"></div>
     <div className="home-col-middle">
-      <div className="home-face-icon"><img src="./images/penguin-face-150.png" style={{width: "150px", height: "150px"}} /> </div>
-      <div className="home-main-text"></div>
-      <div className="home-button"></div>
+      <div className="home-face-icon">
+        <img src="./images/penguin-face-150.png" style={{width: "150px", height: "150px"}}/>
+      </div>
+      <div className="home-main-text">
+        Hola amigos! I'm Paco, a Spanish-language chatbot. I was developed by Gladys Um and am powered by IBM Watson Conversation Service. Want to practice your Español?
+      </div>
+      <div className="home-button-container">
+        <FlatButton className="chat-button" label="Vamos a charlar"/>
+      </div>
     </div>
     <div className="home-col-right"></div>
   </div>
