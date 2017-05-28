4d1331ec8e20535e4541757cf7f564a83e5d2cb4
Gladys Um <gladysum@gmail.com>
2017-05-28 18:31:38 -0400

    wrote my bio

--- browser/About.js
+++ browser/About.js
@@ -16,15 +16,15 @@ const About = () => (
       <div className="about-container-left">
         <div className="bio-left">
           <p id="name">Gladys Um</p>
-          <p>Fullstack software developer proficient in JavaScript, Node.js, React, React-Redux, Express.js, Sequelize, PostgreSQL, Mocha, Chai, Jasmine, HTML, and CSS.
-            Interests: Artificial intelligence, natural language processing, chatbots, IBM Watson Services</p>
+          <p>I'm a fullstack software engineer working in JavaScript, Node, Express, React, React-Redux, HTML, and CSS. In addition to building apps that combine beautiful user interfaces with rigorous backend logic, I enjoy solving algorithmic problems on CodeWars and CoderByte. I have a BA in mathematics from Harvard and an MA in Romance Languages from Princeton.
+            </p>
         </div>
       </div>
 
       <div className="spacing"></div>
 
       <div className="image-container-right">
-        <img src="./images/penguin-square.jpg" style={{width: "340px", height: "340px"}}/>
+        <img src="./images/penguin-square.jpg" style={{width: "280px", height: "280px"}}/>
       </div>
 
       <div className="about-container-right">
