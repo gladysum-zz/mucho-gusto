65260315a6b3df8d13d3e7bc2d5ee1b4173e14fb
Gladys Um <gladysum@gmail.com>
2017-05-28 17:58:20 -0400

    about page styling

--- browser/About.js
+++ browser/About.js
@@ -3,22 +3,39 @@ import {Link} from 'react-router-dom'
 
 const About = () => (
   <div className="about-background">
-    <div className="about-title">
-      Meet the Team
-    </div>
+    <div className="about-col-left"></div>
+    <div className="about-col-middle">
+      <div className="about-title">
+        Meet the Team
+      </div>
 
-    <div className="image-container">
-      <img src="./images/gladys.jpg" style={{width: "330px", height: "330px"}}/>
-    </div>
+      <div className="image-container-left">
+        <img src="./images/gladys.jpg" style={{width: "340px", height: "340px"}}/>
+      </div>
 
-    <div className="about-container">
-      <div className="bio">
-        <p id="name">Gladys Um</p>
-        <p>Fullstack software developer proficient in JavaScript, Node.js, React, React-Redux, Express.js, Sequelize, PostgreSQL, Mocha, Chai, Jasmine, HTML, and CSS.
-          Interests: Artificial intelligence, natural language processing, chatbots, IBM Watson Services</p>
+      <div className="about-container-left">
+        <div className="bio-left">
+          <p id="name">Gladys Um</p>
+          <p>Fullstack software developer proficient in JavaScript, Node.js, React, React-Redux, Express.js, Sequelize, PostgreSQL, Mocha, Chai, Jasmine, HTML, and CSS.
+            Interests: Artificial intelligence, natural language processing, chatbots, IBM Watson Services</p>
+        </div>
+      </div>
+
+      <div className="spacing"></div>
+
+      <div className="image-container-right">
+        <img src="./images/penguin-square.jpg" style={{width: "340px", height: "340px"}}/>
+      </div>
+
+      <div className="about-container-right">
+        <div className="bio-right">
+          <p id="name">Paco</p>
+          <p>Fullstack software developer proficient in JavaScript, Node.js, React, React-Redux, Express.js, Sequelize, PostgreSQL, Mocha, Chai, Jasmine, HTML, and CSS.
+            Interests: Artificial intelligence, natural language processing, chatbots, IBM Watson Services</p>
+        </div>
       </div>
     </div>
-
+    <div className="about-col-right"></div>
   </div>
 )
 
