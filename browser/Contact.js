ac675df8361edb1e1f91d18afb9d04e482bd3b5c
Gladys Um <gladysum@gmail.com>
2017-05-30 23:07:23 -0400

    prettier colors

--- browser/Contact.js
+++ browser/Contact.js
@@ -18,38 +18,38 @@ export default class Contact extends React.Component {
     </div>
 
     <div className="contact-col-middle">
-      <div className="contact-me">
-        Contact Gladys
-      </div>
-      <div className="contact-text">
-        Want to collaborate? Want to learn more about me? <br/> Connect with me via any of the channels below.
-      </div>
-      <div className="contact-links">
-        <div className="contact-col">
-          <div className="social">
-            Email
+      <div className="contact-info">
+        <div className="contact-title">
+          Contact Gladys
+        </div>
+        <div className="contact-text">
+          Want to collaborate? Want to learn more about me? <br/> Connect with me via any of the channels below.
+        </div>
+        <div className="contact-links">
+          <div className="contact-col">
+            <div className="social">
+              Email
+            </div>
+            gladysum (at) gmail (dot) com
           </div>
-          gladysum (at) gmail (dot) com
-        </div>
-        <div className="contact-col">
-          <a href="https://www.linkedin.com/in/gladysum/" target="_blank" className="social">
-            LinkedIn
-          </a><br/>
-          linkedin.com/in/gladysum/
-        </div>
-        <div className="contact-col">
-          <a href="https://github.com/gladysum" target="_blank" className="social">
-            GitHub
-          </a><br/>
-          github.com/gladysum
+          <div className="contact-col">
+            <a href="https://www.linkedin.com/in/gladysum/" target="_blank" className="social">
+              LinkedIn
+            </a><br/>
+            linkedin.com/in/gladysum/
+          </div>
+          <div className="contact-col">
+            <a href="https://github.com/gladysum" target="_blank" className="social">
+              GitHub
+            </a><br/>
+            github.com/gladysum
+          </div>
         </div>
       </div>
     </div>
 
     <div className="contact-col-right">
     </div>
-
-
   </div>
     )
   }
